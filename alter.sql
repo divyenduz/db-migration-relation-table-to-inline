a4115dd50e79cf93b23362f0f8f884b971e98ac2
Divyendu Singh <singh@prisma.io>
2019-05-14 11:02:10 +0200

    fix

--- alter.sql
+++ alter.sql
@@ -1,15 +1,20 @@
 ALTER TABLE
   User
 ADD
-  COLUMN address CHAR(25) COLLATE utf8 NOT NULL;
-
+  COLUMN address CHAR(25) CHARACTER SET utf8;
+ 
 UPDATE
   User
   INNER JOIN _AddressToUser ON _AddressToUser.B = User.id
+SET
+  User.address = _AddressToUser.A
 
 ALTER TABLE
   User
-ADD FOREIGN KEY(address) REFERENCES Address(`id`);
+ADD
+  CONSTRAINT FOREIGN KEY(address) REFERENCES Address(id);
 
-SET
-  User.address = _AddressToUser.A;
+ALTER TABLE
+  User
+MODIFY
+  address CHAR(25) CHARACTER SET utf8 NOT NULL;
